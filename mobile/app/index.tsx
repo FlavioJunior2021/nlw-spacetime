dea09495446bbfee23bc0c35923c6575925fab02
Flavinho <flavio.junior@escolar.ifrn.edu.br>
2023-05-18 20:05:18 -0300

    login whint github

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,27 +1,75 @@
+/* eslint-disable prettier/prettier */
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useRouter } from 'expo-router'
+import { useEffect } from 'react'
+
+import * as SecureStore from 'expo-secure-store'
+
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
 
-import blurBg from './src/assets/bg-blur.png'
-import NlwLogoSvg from './src/assets/nlw-logo.svg'
-import Stripes from './src/assets/stripes.svg'
+import blurBg from '../src/assets/bg-blur.png'
+import NlwLogoSvg from '../src/assets/nlw-logo.svg'
+import Stripes from '../src/assets/stripes.svg'
 
 import {
-  Roboto_400Regular,
-  Roboto_700Bold,
-  useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+    useFonts,
 } from '@expo-google-fonts/roboto'
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import { styled } from 'nativewind'
+import { api } from '../src/lib/api'
 
 const StyledStripes = styled(Stripes)
 
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/d9f41e13cf87e1670326 ',
+}
+
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [, response, signinWithGithub] = useAuthRequest(
+    {
+      clientId: 'd9f41e13cf87e1670326',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'spacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+
+      api
+        .post('/register', {
+          code,
+        })
+        .then((response) => {
+          const { token } = response.data
+          SecureStore.setItemAsync('token', token)
+          router.push('/memories')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }, [response])
+
   if (!hasLoadedFonts) {
     return null
   }
@@ -47,6 +95,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signinWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             COMEÇAR A CADASTRAR
